109d8b85d72289400d26ab36aaec5a7682fc2b7f
Yun Ding <yding8@lsu.edu>
2016-09-02 16:58:58 -0500

    recursive programming assignments passed tests
    	modified:   recfun/src/main/scala/recfun/Main.scala

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,65 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if ((c == 0) || (r == c)) {
+      1
+    }
+    else {
+      pascal(c, r - 1) + pascal(c - 1, r - 1)
+    }
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def loop(left: List[Char], right: List[Char]): List[Char] = {
+      if (right.length == 0) {
+        left
+      }
+      else {
+        if (right.head == '(') {
+          loop('(' :: left, right.tail)
+        }
+        else if (right.head == ')') {
+          if (left.length != 0 && left.head == '(') {
+            loop(left.tail, right.tail)
+          }
+          else {
+            loop(')' :: left, right.tail)
+          }
+        }
+        else
+          loop(left, right.tail)
+      }
+    }
+
+    val left = List[Char]()
+    val after_loop = loop(left, chars)
+    after_loop.length == 0
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty) {
+      0
+    }
+    else if (money == 0) {
+      1
+    }
+    else {
+      val first_coin = coins.head
+      if (first_coin > money) {
+        countChange(money, coins.tail)
+      }
+      else {
+        countChange(money - first_coin, coins) + countChange(money, coins.tail)
+      }
+    }
+  }
+}
