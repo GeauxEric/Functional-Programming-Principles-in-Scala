c8c3089d14ff0cd19ffb632d5eaf1a2b239cef96
Yun Ding <yding8@lsu.edu>
2016-09-12 20:36:17 -0500

    completes assignment: Object-Oriented Sets
    	modified:   objsets/src/main/scala/objsets/TweetSet.scala

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -34,6 +34,8 @@ class Tweet(val user: String, val text: String, val retweets: Int) {
  */
 abstract class TweetSet {
 
+  def isEmpty: Boolean
+
   /**
    * This method takes a predicate and returns a subset of all the elements
    * in the original set for which the predicate is true.
@@ -41,7 +43,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty())
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -54,7 +56,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
+  def union(that: TweetSet): TweetSet = ???
   
   /**
    * Returns the tweet from this set which has the greatest retweet count.
@@ -76,7 +78,8 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def descendingByRetweet: TweetList = ???
+
+  def descendingByRetweet: TweetList = ???
   
   /**
    * The following methods are already implemented
@@ -107,8 +110,17 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+
+  def isEmpty: Boolean = true
+
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
+
+  override def union(that: TweetSet): TweetSet = that
+
+  override def mostRetweeted: Tweet = throw new java.util.NoSuchElementException
+
+  override def descendingByRetweet = Nil
+
   /**
    * The following methods are already implemented
    */
@@ -124,9 +136,33 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-    
+  def isEmpty: Boolean = false
+
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+    if (p(elem)) left.filterAcc(p, right.filterAcc(p, acc.incl(elem)))
+    else left.filterAcc(p, right.filterAcc(p, acc))
+  }
+
+  override def union(that: TweetSet): TweetSet = {
+    if (that.contains(elem)) left.union(right.union(that))
+    else (left.union(right.union(that.incl(elem))))
+  }
+
+  def max(a: Tweet, b: Tweet) =
+    if (a.retweets >= b.retweets) a else b
+
+  override def mostRetweeted: Tweet = {
+    if (left.isEmpty && right.isEmpty) elem
+    else if (left.isEmpty) max(elem, right.mostRetweeted)
+    else if (right.isEmpty) max(elem, left.mostRetweeted)
+    else max(max(right.mostRetweeted, left.mostRetweeted), elem)
+  }
+
+  override def descendingByRetweet = {
+    val m = mostRetweeted
+    new Cons(m, remove(m).descendingByRetweet)
+  }
+
   /**
    * The following methods are already implemented
    */
@@ -180,14 +216,14 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-    lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet = allTweets.filter(t => google.exists(t.text.contains))
+  lazy val appleTweets: TweetSet = allTweets.filter(t => apple.exists(t.text.contains))
   
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-     lazy val trending: TweetList = ???
+  lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
   }
 
 object Main extends App {
